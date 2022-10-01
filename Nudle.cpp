75aa69444afa23db5194def2ca9be762171b14aa
Erick Vilcica <erick.vilcica@protonmail.com>
2022-10-01 21:59:36 +0100

    create working password generator for given length and fix some errors

--- Nudle.cpp
+++ Nudle.cpp
@@ -1,12 +1,13 @@
 ﻿// Nudle.cpp : This file contains the 'main' function. Program execution begins and ends there.
 #include <fstream>
-#include <iostream> 
+#include <iostream>
+#include <stdlib.h>
 #include "Header.h"  /* Header Protocol*/
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
+    long length = strtol(argv[1], NULL, 10);
 	cout << "NUDLE\n";
-	cout << PassGen() << endl;
-
+	cout << PassGen(length) << endl;
 }
--- PasGen.cpp
+++
@@ -1,6 +0,0 @@
-#include "Header.h"
-static const char alphnum[] = "0123456789" "!@#$%^&*" "ABCDEFGHIJKLMNOPQRSTUVWXYZ" "abcdefghijklmnopqrstuvwxyz";
-int PassGen()
-{
-	return 123;
-}
